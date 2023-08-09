2bce0724aa5135071b14e5669b32155010132c82
Jhayr <dmz.gfx@gmail.com>
2023-08-09 22:50:36 +0800

    Create first_challenge.php
    A function that will get largest element in an array

---
+++ first_challenge.php
@@ -0,0 +1,17 @@
+<?php
+
+function getLargestElement($set, $position, $from = 'end') {
+
+    $from === 'start' ? sort($set) : rsort($set);
+
+    return $set[$position - 1] ?? false;
+}
+
+$test = [3, 2, 1, 5, 6, 4];
+
+echo(getLargestElement($test, 2));
+echo(getLargestElement($test, 3, 'start'));
+echo(getLargestElement($test, 1));
+echo(getLargestElement($test, 0));
+echo(getLargestElement($test, 9));
+echo(getLargestElement($test, -1));
